fd926a1c6a384f4a8b5707fdb0687733a60ca94d
Depanri Purba <depanri.purba@gmail.com>
2025-04-05 08:53:02 +0700

    perbaiki kalimat

--- application/views/admin/datapasien.php
+++ application/views/admin/datapasien.php
@@ -8,7 +8,7 @@
                         <div class="container-fluid">
                             <div class="row mb-2">
                                 <div class="col-sm-6">
-                                    <h1 class="m-0">Data Pasien</h1>
+                                    <h1 class="m-0">Data Pengguna</h1>
                                 </div><!-- /.col -->
                             </div><!-- /.row -->
                         </div><!-- /.container-fluid -->
@@ -26,9 +26,6 @@
                                     <tr>
                                         <th style="width:10px">No.</th>
                                         <th style="width:240px">Nama Pasien</th>
-                                        <th style="width:100px">Usia</th>
-                                        <th style="width:130px">Jenis Kelamin</th>
-                                        <th>Alamat</th>
                                         <th style="width:120px">Aksi</th>
                                     </tr>
                                 </thead>
@@ -37,9 +34,6 @@
                                     <tr>
                                         <td><?=$no++?></td>
                                         <td><?=$p['fullname']?></td>
-                                        <td><?=$p['usia']?> Tahun</td>
-                                        <td><?=$p['jenis_kelamin']?></td>
-                                        <td><?=$p['alamat']?></td>
                                         <td>
                                             <a onclick='confirm("Apakah Anda Ingin Menghapus Data Pasien ini?")' href='<?=base_url('master/delete_pasien/'.$p['id'])?>' class="btn btn-danger">
                                                 <span>Hapus</span>
